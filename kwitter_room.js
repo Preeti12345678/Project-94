63bddf723e1ec5b4ce4a8cb1f0cda225f9ad1602
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-03-29 19:41:17 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBlrfQflWVERJ6FiygmN86ImxV0v0KkhPU",
+    authDomain: "kwitter-2d701.firebaseapp.com",
+    databaseURL: "https://kwitter-2d701-default-rtdb.firebaseio.com",
+    projectId: "kwitter-2d701",
+    storageBucket: "kwitter-2d701.appspot.com",
+    messagingSenderId: "268592771083",
+    appId: "1:268592771083:web:6a430136bce3fdcdda570e",
+    measurementId: "G-QXBMTLHG5L"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
