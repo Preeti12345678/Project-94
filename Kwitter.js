1f1c15c5b53e7ce9b5646d9f0eb8b1e3d4bd9932
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-03-30 15:28:22 +0530

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,5 +1,5 @@
 function addUser(){
     user_name=document.getElementById("input_user").value;
     localStorage.setItem("User_name", user_name);
-    window.location.replace("kwitter_room.html");
+    window.location.replace("Kwitter_room.html");
 }
